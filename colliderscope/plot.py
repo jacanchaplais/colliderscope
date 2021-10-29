9bad2f4ea3909f3dda093660cc739be29cdbed05
Jacan Chaplais <jlc1n20@soton.ac.uk>
2021-10-29 19:37:25 +0100

    general purpose pyvis shower functional

--- colliderscope/plot.py
+++ colliderscope/plot.py
@@ -4,76 +4,105 @@ import numpy as np
 
 
 class ShowerDAG:
-    from heparchy.data import ShowerData as __ShowerData
-    from pyvis.network import __Network
+    import pandas as __pd
+    from pyvis.network import Network as __Network
+    from mcpid.lookup import PdgRecords as __PdgRecords
+    from colour import Color as __Color
 
-    def __init__(self, shower: __ShowerData, masks: dict):
-        df = shower.to_pandas(data=['pdg', 'final', 'edges'])
-        for key, mask in masks.items():
-            df[key] = mask
-        self.__df = df
-        self.shower = shower
+
+    def __init__(self, edges: np.ndarray, pdg: np.ndarray, pt: np.ndarray,
+                 final: np.ndarray, masks: dict = dict()):
+        data = {'in_edge': edges['in'],
+                'out_edge': edges['out'],
+                'pt': pt,
+                'final': final,
+                'pdg': pdg,
+                'background': False,
+               }
+        data.update(masks)
+        self.__mask_keys = list(masks.keys())
+        self.__df = self.__pd.DataFrame(data)
+        self.__pdg_lookup = self.__PdgRecords()
+        self.table = self.__df
+        self.__br = '<br />'
 
     @classmethod
-    def from_numpy(cls, edges: np.ndarray, pmu: np.ndarray, final: np.ndarray,
-                   pdg: np.ndarray, masks: dict):
-        shower = self.__ShowerData(edges, pmu, final, pdg)
-        return cls(shower, masks)
+    def from_heparchy(cls, shower, masks: dict = dict()):
+        return cls(edges=shower.edges, pt=shower.pt(), final=shower.final,
+                   pdg=shower.pdg, masks=masks)
 
-    def __pcl_listing(self, names):
-        names = list(names)
-        names.sort()
-        return br.join(wrapper.wrap(', '.join(names))) + br
-
-    self.__heading = lambda text: '<b>' + text + ':</b>' + br
-
-    def __vtx_title(self, pdgs_in, pdgs_out, length=40):
+    def __pcl_listing(self, names, length=40):
         wrapper = textwrap.TextWrapper(
             width=length,
             break_long_words=False,
             break_on_hyphens=False,
             )
+        names = list(names)
+        names.sort()
+        return self.__br.join(wrapper.wrap(', '.join(names))) + self.__br
+
+    def __heading(self, text):
+        return '<b>' + text + ':</b>' + self.__br
+
+    def __vtx_title(self, pdgs_in, pdgs_out):
         num_in = len(pdgs_in)
         num_out = len(pdgs_out)
-        names_in = map(pdg_to_name, pdgs_in)
-        names_out = map(pdg_to_name, pdgs_out)
-        br = '<br />'
+        names_in = map(self.__pdg_to_name, pdgs_in)
+        names_out = map(self.__pdg_to_name, pdgs_out)
         return str(
-            heading(f'particles in ({num_in})')
-            + pcl_listing(names_in)
-            + br
-            + heading(f'particles out ({num_out})')
-            + pcl_listing(names_out)
+            self.__heading(f'particles in ({num_in})')
+            + self.__pcl_listing(names_in)
+            + self.__br
+            + self.__heading(f'particles out ({num_out})')
+            + self.__pcl_listing(names_out)
             )
 
+    def __pdg_to_name(self, pdg) -> str:
+        pdg = list([pdg])
+        name = self.__pdg_lookup.properties(pdgs=pdg, props=['name'])
+        name = name['name']
+        return name.item()
+
     def to_pyvis(self):
-        pdg_to_name = lambda pdg: Particle.from_pdgid(pdg).name
-        shower = Network('640px', '960px', notebook=True, directed=True)
-        W_edges = partons['W'][['in_edge', 'out_edge']]
+        shower = self.__Network('640px', '960px', notebook=True, directed=True)
+        partons = {mask_key: self.__df.query(mask_key)
+                   for mask_key in self.__mask_keys
+                   }
+        colours = {mask_key: self.__Color(pick_for=mask_key).get_web()
+                   for mask_key in self.__mask_keys}
+        bg_q = ' and '.join(map(lambda key: f'(not {key})', self.__mask_keys))
+        # print(bg_q)
+        bg_df = self.__df.query(bg_q)
+        bg_idxs = bg_df.index
+        # print(bg_idxs)
+        self.__df.loc[bg_idxs, 'background'] = True
+        partons.update({'background': bg_df})
+        colours.update({'background': '#000000'})
+        shape = 'dot'
+        leaf_shape = 'star'
+        parton_edges = []
         for parton, parton_data in partons.items():
+            print(parton)
             colour = colours[parton]
             for num, pcl in parton_data.iterrows():
                 node = int(pcl['in_edge'])
                 next_node = int(pcl['out_edge'])
-                in_edges = data.query('@node == out_edge')
+                in_edges = self.__df.query('@node == out_edge')
                 in_pdgs = in_edges['pdg']
-                out_edges = data.query('@node == in_edge')
+                out_edges = self.__df.query('@node == in_edge')
                 out_pdgs = out_edges['pdg']
-                title = vtx_title(pdgs_in=in_pdgs, pdgs_out=out_pdgs)
+                title = self.__vtx_title(pdgs_in=in_pdgs, pdgs_out=out_pdgs)
                 label = ' '
-                shape = 'dot'
-                leaf_shape = 'star'
                 if pcl['final']:
                     leaf_node = next_node
-                    leaf_label = pdg_to_name(pcl['pdg'])
+                    leaf_label = self.__pdg_to_name(pcl['pdg'])
                     shower.add_node(leaf_node, label=leaf_label,
                                     shape=leaf_shape, color=colour)
                 shower.add_node(node, label=label, title=title, shape=shape,
                                 size=10, color=colour)
-
             in_edge = map(int, parton_data['in_edge'])
             out_edge = map(int, parton_data['out_edge'])
-            edges = zip(in_edge, out_edge)
-
+            parton_edges += [zip(in_edge, out_edge)]
+        for edges in parton_edges:
             shower.add_edges(edges)
         shower.show('shower.html')
