6ddb99f6959ccdd0f1333fdb33ee33aba1988c8b
Jacan Chaplais <jlc1n20@soton.ac.uk>
2021-11-05 12:21:42 +0000

    refactored using pandas for visjs general output

--- colliderscope/plot.py
+++ colliderscope/plot.py
@@ -25,6 +25,7 @@ class ShowerDAG:
         self.__pdg_lookup = self.__PdgRecords()
         self.table = self.__df
         self.__br = '<br />'
+        self.__eps = 1e-8
 
     @classmethod
     def from_heparchy(cls, shower, masks: dict = dict()):
@@ -52,42 +53,106 @@ class ShowerDAG:
             + self.__pcl_listing(names)
             )
 
-    def vis_nodes(self):
+    def __hex_to_rgb_column(self, hex_col: np.ndarray) -> np.ndarray:
+        colors, color_idxs = np.unique(hex_col, return_inverse=True)
+        rgbs = np.array([self.__Color(str(hex_)).rgb for hex_ in colors])
+        rgb_col = rgbs[color_idxs]
+        return rgb_col
+
+    def __rgb_to_hex_column(self, rgb_col: np.ndarray) -> np.ndarray:
+        colors, color_idxs = np.unique(rgb_col, axis=0, return_inverse=True)
+        hexs = np.array([self.__Color(rgb=tuple(rgb)).hex for rgb in colors])
+        hex_col = hexs[color_idxs]
+        return hex_col
+
+    def __add_hex_columns(self, df_cols: list) -> np.ndarray:
+        rgb_cols = [self.__hex_to_rgb_column(hex_col) for hex_col in df_cols]
+        rgb_sum = np.sum(rgb_cols, axis=0)
+        norm = np.linalg.norm(rgb_sum, axis=1, keepdims=True)
+        rgb_mix = np.divide(rgb_sum, norm, where=(np.abs(norm)>self.__eps))
+        hex_mix = self.__rgb_to_hex_column(rgb_mix)
+        return hex_mix
+
+    def _vis_nodes(self):
+        # prevent accidentally mutation of instance data
         df = self.__df.copy()
+        # add column of names
         df['name'] = self.__pdg_to_name(df['pdg'])
+        # node colours
+        mask_keys = self.__mask_keys
+        colors = {ma_key: self.__Color(pick_for=ma_key).get_web()
+                  for ma_key in mask_keys}
+        # construct vertex titles and whether vertices are leaves
+        aggfuncs = {'name': lambda x: self.__vtx_title(x, out=True)}
+        hrtg_agg_funcs = {ma_key: lambda x: x.any() for ma_key in mask_keys}
+        aggfuncs.update(hrtg_agg_funcs)
         edges_out = df.pivot_table(
-                'name',
+                ['name'] + list(mask_keys),
                 index='in_edge',
-                aggfunc=lambda x: self.__vtx_title(x, out=True),
+                aggfunc=aggfuncs,
                 )
+        aggfuncs={
+            'name': lambda x: self.__vtx_title(x, out=False),
+            'final': lambda x: x.size == 1 and x.all(),
+            }
+        aggfuncs.update(hrtg_agg_funcs)
         edges_in = df.pivot_table(
-                ['name', 'final'],
+                ['name', 'final'] + list(mask_keys),
                 index='out_edge',
-                aggfunc={
-                    'name': lambda x: self.__vtx_title(x, out=False),
-                    'final': lambda x: x.size == 1 and x.all(),
-                    }
+                aggfunc=aggfuncs,
                 )
+        # gather labels for leaf nodes
+        labels = df.query('final')
+        labels = labels.pivot_table(
+                values='name', index='out_edge', aggfunc=lambda x: x)
+        labels.index.name = 'id'
+        labels.rename(columns={'name': 'label'}, inplace=True)
         final = edges_in['final']
-        node_df = edges_out + edges_in
+        # append the titles for edges in and out of the vertices
+        node_df = edges_in.join(edges_out, lsuffix='_in', rsuffix='_out',
+                                how='outer')
+        for ma_key in mask_keys:
+            ma_key = str(ma_key)
+            key_in = ma_key + '_in'
+            key_out = ma_key + '_out'
+            color_mask = node_df[key_in] | node_df[key_out]
+            node_df[ma_key] = '#000000'
+            node_df.loc[(color_mask, ma_key)] = colors[ma_key]
+            node_df.drop(columns=[key_in, key_out], inplace=True)
+        color_blend = self.__add_hex_columns(
+                [node_df[mask_key] for mask_key in mask_keys])
+        node_df.drop(columns=mask_keys, inplace=True)
+        node_df['color'] = color_blend
+        node_df['title'] = node_df['name_in'] + node_df['name_out']
+        node_df.drop(columns=['name_in', 'name_out'], inplace=True)
         node_df['final'] = final
+        # set the appropriate column headers and index names for pyvis
         node_df.index.name = 'id'
         node_df.rename(columns={'name': 'title'}, inplace=True)
+        # add labels for final state particles
+        node_df = node_df.join(labels, how='outer')
+        # replace null values of titles and labels with empty strings
+        node_df['title'].fillna(value='', inplace=True)
+        node_df['label'].fillna(value='', inplace=True)
+        # set visualisation defaults for nodes
         node_df['size'] = 10
         node_df['shape'] = final.apply(
                 lambda x: 'star' if x == True else 'dot')
+        # settings for root node (start of event)
         root_id = int(node_df.index.values[node_df['final'].isnull()])
         node_df.loc[(root_id, 'final')] = False
         node_df.loc[(root_id, 'title')] = 'start of event'
         node_df.loc[(root_id, 'shape')] = 'square'
         node_df.loc[(root_id, 'size')] = 20
-        return node_df
-        # node_df = edges_in.join(edges_out, lsuffix='_in', rsuffix='_out')
-        # node_df.index.name = 'id'
-        # node_df = node_df.dropna()
-        # pcl_names = node_df.apply(title, axis=0)
-        # title_series = title_series.apply(title, axis=1)
-        # return title_series
+        node_df.drop(columns='final', inplace=True)
+        node_df.reset_index(inplace=True)
+        return node_df.to_dict('records')
+
+    def _vis_edges(self):
+        df = self.__df[['in_edge', 'out_edge']].copy()
+        df.rename(columns={'in_edge': 'from', 'out_edge': 'to'}, inplace=True)
+        df['arrows'] = 'to'
+        return df.to_dict('records')
 
     def __pdg_to_name(self, pdgs) -> list:
         if isinstance(pdgs, int):
@@ -100,50 +165,8 @@ class ShowerDAG:
 
     def to_pyvis(self, notebook=True):
         shower = self.__Network('640px', '960px', notebook=notebook, directed=True)
-        partons = {mask_key: self.__df.query(mask_key)
-                   for mask_key in self.__mask_keys
-                   }
-        colours = {mask_key: self.__Color(pick_for=mask_key).get_web()
-                   for mask_key in self.__mask_keys}
-        bg_q = ' and '.join(map(lambda key: f'(not {key})', self.__mask_keys))
-        # print(bg_q)
-        bg_df = self.__df.query(bg_q)
-        bg_idxs = bg_df.index
-        # print(bg_idxs)
-        self.__df.loc[bg_idxs, 'background'] = True
-        partons.update({'background': bg_df})
-        colours.update({'background': '#000000'})
-        shape = 'dot'
-        leaf_shape = 'star'
-        parton_edges = []
-        for parton, parton_data in partons.items():
-            colour = colours[parton]
-            for num, pcl in parton_data.iterrows():
-                node = int(pcl['in_edge'])
-                next_node = int(pcl['out_edge'])
-                in_edges = self.__df.query('@node == out_edge')
-                in_pdgs = in_edges['pdg']
-                out_edges = self.__df.query('@node == in_edge')
-                out_pdgs = out_edges['pdg']
-                title = self.__vtx_title(names=self.__pdg_to_name(in_pdgs), out=False)
-                title += str(self.__br
-                             + self.__vtx_title(names=self.__pdg_to_name(in_pdgs), out=False)
-                             )
-                label = ' '
-                if pcl['final']:
-                    leaf_node = next_node
-                    leaf_label = self.__pdg_to_name(pcl['pdg'])[-1]
-                    shower.add_node(leaf_node, label=leaf_label,
-                                    shape=leaf_shape, color=colour)
-                shower.add_node(node, label=label, title=title, shape=shape,
-                                size=10, color=colour)
-            in_edge = map(int, parton_data['in_edge'])
-            out_edge = map(int, parton_data['out_edge'])
-            parton_edges += [zip(in_edge, out_edge)]
-        for edges in parton_edges:
-            shower.add_edges(edges)
-        # print(shower.nodes)
-        # print(shower.edges)
+        shower.nodes = self._vis_nodes()
+        shower.edges = self._vis_edges()
         vis = shower.show('shower.html')
         self.shower = shower
         if notebook == True:
