38f336f741b5d0ad205a0cdd2f4ad8b0da84e892
Jacan Chaplais <jacan.chaplais@gmail.com>
2022-06-09 11:22:33 +0100

    added option to align histogram, and calc pdf

--- colliderscope/data.py
+++ colliderscope/data.py
@@ -6,9 +6,15 @@ Data structures for formatting and pre-processing data in preparation
 for plotting.
 """
 from dataclasses import dataclass, field
-from typing import Tuple
+from enum import Enum
+from typing import Tuple, Optional
 
 import numpy as np
+
+
+class Align(Enum):
+    CENTER = "center"
+    LEFT = "left"
 
 
 @dataclass
@@ -20,30 +26,66 @@ class Histogram:
     ----------
     num_bins : int
         Number of bins to store multiplicities within.
-    expected : float
+    window : tuple[float, float]
+        Range of x-axis to use for binning.
+    _align : str
+        Alignment of x-axis. Valid options are "left" or "center".
+        If "left", `window` will be relative to `x=0`. If "center",
+        `window` will be relative to `expected`.
+        Default is left.
+    expected : float, optional
         Expected value from theory for a peak.
-    window_width : float
-        The range around the expected value to bin.
-        ie. x_range = expected +/- window_width.
+
+    Attributes
+    ----------
+    num_bins : int
+    expected : float
+    window : tuple[float, float]
+    align : Align
+    x_range : tuple
+        Low and high boundaries of the binned range.
+    counts : ndarray
+        Integer counts of values which entered each bin.
+    bin_edges : ndarray
+        Boundaries defining all bins.
+    pdf : tuple[ndarray, ndarray]
+        Bin centers and count density. May be used for bar chart plots.
     """
     num_bins: int
-    expected: float
-    window_width: float
-    x_range: Tuple[float, float] = field(init=False)
+    window: Tuple[float, float]
+    _align: str = "left"
+    expected: Optional[float] = None
     counts: np.ndarray = field(init=False)
     bin_edges: np.ndarray = field(init=False)
+    _total: int = field(init=False, repr=False, default=0)
     
     def __post_init__(self):
-        self.x_range = (self.expected - self.window_width,
-                        self.expected + self.window_width)
+        self.align = Align(self._align)
+        self.x_range = self.window
+        if self.align is Align.CENTER:
+            if self.expected is None:
+                raise ValueError(
+                        "You must specify `expected` in order to use "
+                        "center alignment."
+                        )
+            self.x_range = (self.expected + self.window[0],
+                            self.expected + self.window[1])
         self.counts = np.zeros(self.num_bins, dtype='<i')
         self.bin_edges = np.linspace(*self.x_range, self.num_bins + 1)
+        self.bin_edges = self.bin_edges.squeeze()
     
     def update(self, val: float) -> None:
         """Records a new value to the binned counts."""
+        self._total += 1
         idx = np.digitize(val, self.bin_edges) - 1
-        if idx == -1:
+        if idx <= -1:
             return
-        if idx == self.num_bins:
+        if idx >= self.num_bins:
             return
         self.counts[idx] += 1
+
+    @property
+    def pdf(self) -> Tuple[np.ndarray, np.ndarray]:
+        """Provides tuple of bin centers and count density."""
+        return ((self.bin_edges[1:] + self.bin_edges[:-1]) / 2.0,
+                self.counts / self._total)
